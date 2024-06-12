300e6b8ecc333d0f7ba007bcbc1ee10fa20ec9d6
Ben Jones <benjones1864@gmail.com>
2024-06-12 16:34:36 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from './models/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,82 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ChirpusPizza';
+
+  menu: MenuItem[] = [
+    {
+      name: "Pepperoni Pizza Slice",
+      category: "Entree",
+      price: 3,
+      vegetarian: false,
+      soldOut: false
+    },
+    {
+      name: "Cheese Pizza Slice",
+      category: "Entree",
+      price: 2.75,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Pepperoni Pizza",
+      category: "Entree",
+      price: 11,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Cheese Pizza",
+      category: "Entree",
+      price: 10,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Cheeseburger",
+      category: "Entree",
+      price: 6,
+      vegetarian: false,
+      soldOut: false
+    },
+    {
+      name: "French Fries",
+      category: "Side",
+      price: 2,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Cheese Bread",
+      category: "Side",
+      price: 2.50,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Ice Cream",
+      category: "Dessert",
+      price: 4,
+      vegetarian: true,
+      soldOut: false
+    },
+    {
+      name: "Ice Cream w/ Bacon",
+      category: "Dessert",
+      price: 6,
+      vegetarian: false,
+      soldOut: true
+    },
+  ];
+
+  cart: MenuItem[] = [];
+
+  addToCart(m: MenuItem): void {
+    this.cart.push(m);
+    console.log(this.cart);
+  }
+
+  getTotal(): number {
+    return this.cart.reduce((total, next) => total + next.price, 0);
+  }
 }
+
