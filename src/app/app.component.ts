b80dc3807deabcce49c9dea5e02a825fd7bbd589
Ben Jones <benjones1864@gmail.com>
2024-06-13 09:16:53 -0400

    added CurrencyPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuItem } from './models/menu-item';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
